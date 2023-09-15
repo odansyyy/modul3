aa74412b554a255446f8aa9ff223cbf31882cd1f
odansyyy <odansyaharta26@gmail.com>
2023-09-15 16:06:56 +0700

    pertama

---
+++ src/Component/HelloComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import'./HelloComponent.css' ;
+
+const HelloComponent = () => {
+    return <p className='text-p'> ini adalah arrow function yang ada di folder </p>
+}
+
+//agar component ini dapat dipakai dimana aja 
+export default HelloComponent;
